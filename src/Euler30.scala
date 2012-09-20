b5055aeb137682139c503ad2f9e8b9d0ab3fa5cd
Giovanni Caporaletti <gcaporaletti@gmail.com>
2012-09-20 15:27:22 +0100

    25

--- src/Euler30.scala
+++ src/Euler30.scala
@@ -3,14 +3,26 @@ object Resolve30 {
     //    println("21: " + (Euler21 resolve))
     //    println("22: " + (Euler22 resolve))
     //    println("23: " + (Euler23 resolve))
-    println("24: " + (Euler24 resolve))
-    //    println("25: " + (Euler25 resolve))
+    //    println("24: " + (Euler24 resolve))
+    println("25: " + (Euler25 resolve))
     //    println("26: " + (Euler26 resolve))
     //    println("27: " + (Euler27 resolve))
     //    println("28: " + (Euler28 resolve))
     //    println("29: " + (Euler29 resolve))
     //    println("30: " + (Euler30 resolve))
   }
+}
+
+object Euler25 {
+  def fibWithDigits(numDigits: Int) = {
+    def fibWithDigitsTr(curr: BigInt, prev: BigInt, idx:BigInt): BigInt = curr match {
+      case x if x.toString.length >= numDigits => idx+1
+      case _ => fibWithDigitsTr(curr + prev, curr, idx+1)
+    }
+    fibWithDigitsTr(1, 1, 1)
+  }
+
+  def resolve = fibWithDigits(1000)
 }
 
 object Euler24 {
