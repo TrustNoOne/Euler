3b16d04fd67104c307a1d5d8bb9a27ffe44b459d
Giovanni Caporaletti <gcaporaletti@gmail.com>
2012-09-29 18:22:49 +0200

    32

--- src/Euler40.scala
+++ src/Euler40.scala
@@ -2,16 +2,24 @@ import Euler._
 
 object Euler31to40 {
   def main(args: Array[String]) {
-    println("31: " + elapsed(Euler31.solve))
-    //    println("32: " + (Euler32.solve))
-    //    println("33: " + (Euler33.solve))
-    //    println("34: " + (Euler34.solve))
-    //    println("35: " + (Euler35.solve))
-    //    println("36: " + (Euler36.solve))
-    //    println("37: " + (Euler37.solve))
-    //    println("38: " + (Euler38.solve))
-    //    println("39: " + (Euler39.solve))
-    //    println("40: " + (Euler40.solve))
+    //    println("31: " + elapsed(Euler31.solve))
+    println("32: " + elapsed(Euler32.solve))
+    //    println("33: " + elapsed(Euler33.solve))
+    //    println("34: " + elapsed(Euler34.solve))
+    //    println("35: " + elapsed(Euler35.solve))
+    //    println("36: " + elapsed(Euler36.solve))
+    //    println("37: " + elapsed(Euler37.solve))
+    //    println("38: " + elapsed(Euler38.solve))
+    //    println("39: " + elapsed(Euler39.solve))
+    //    println("40: " + elapsed(Euler40.solve))
+  }
+}
+
+object Euler32 {
+  def solve = {
+    val prods = for (a <- Range(1, 100).view; b <- Range(1, 10000).view if a * b < 9876)
+      yield (a * b, "" + a + b + (a * b))
+    prods.filter(x => x._2.length == 9 && !x._2.contains('0') && x._2.toSet.size == 9).map(_._1).toSet.sum
   }
 }
 
