c752453539750b1b868a8f575712cacc2ab305ac
Giovanni Caporaletti <gcaporaletti@gmail.com>
2013-03-10 23:31:46 +0100

    43

--- src/Euler50.scala
+++ src/Euler50.scala
@@ -3,8 +3,8 @@ import Euler._
 object Euler41to50 {
   def main(args: Array[String]) {
     //    println("41: " + elapsed(Euler41.solve()))
-    println("42: " + elapsed(Euler42.solve()))
-    //    println("43: " + elapsed(Euler43.solve()))
+    //    println("42: " + elapsed(Euler42.solve()))
+    println("43: " + elapsed(Euler43.solve()))
     //    println("44: " + elapsed(Euler44.solve()))
     //    println("45: " + elapsed(Euler45.solve()))
     //    println("46: " + elapsed(Euler46.solve()))
@@ -12,6 +12,21 @@ object Euler41to50 {
     //    println("48: " + elapsed(Euler48.solve()))
     //    println("49: " + elapsed(Euler49.solve()))
     //    println("50: " + elapsed(Euler50.solve()))
+  }
+}
+
+object Euler43 {
+  def subStringDivCheck(xs: Seq[Long]) = {
+    fromDigits(xs.slice(6, 9)) % 2 == 0 &&
+      fromDigits(xs.slice(5, 8)) % 3 == 0 &&
+      fromDigits(xs.slice(4, 7)) % 5 == 0 &&
+      fromDigits(xs.slice(3, 6)) % 7 == 0 &&
+      fromDigits(xs.slice(2, 5)) % 11 == 0 &&
+      fromDigits(xs.slice(1, 4)) % 13 == 0 &&
+      fromDigits(xs.slice(0, 3)) % 17 == 0
+  }
+  def solve() = {
+    List(0L, 1, 2, 3, 4, 5, 6, 7, 8, 9).permutations.filter(subStringDivCheck).map(fromDigits).sum
   }
 }
 
