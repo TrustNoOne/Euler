604ae0f8778aaa360c95c70998e44c950ff1e28c
Giovanni Caporaletti <gcaporaletti@gmail.com>
2013-07-03 16:24:10 +0200

    59

---
+++ src/main/scala/euler/til60/Euler59.scala
@@ -0,0 +1,31 @@
+package euler
+package til60
+
+import Utils._
+
+object Euler59 extends EulerProblem {
+
+  val keys = for {
+    x <- 'a' to 'z'
+    y <- 'a' to 'z'
+    z <- 'a' to 'z'
+  } yield Seq(x, y, z)
+
+  implicit def strToIntSeq(s: String) = s.toCharArray.map(_.toInt)
+
+  def decrypt(text: Seq[Char], key: Seq[Char]) = {
+    text.grouped(key.size).flatMap(_.zip(key).map(x => (x._1 ^ x._2).toChar))
+  }
+
+  override def result = withResource("cipher1.txt") { src =>
+    val chars = src.mkString.split(",").map(_.trim.toInt.toChar)
+
+    val decrypted = keys.view.map(k => decrypt(chars, k).toSeq).find { text =>
+      text.containsSlice("this") && text.containsSlice("that")
+    }.head
+
+    // clear text:  decrypted.mkString
+    decrypted.foldLeft(0)(_ + _)
+  }
+
+}
