786db1dd023e17d3096147dd3e1244188f292d16
Giovanni Caporaletti <gcaporaletti@gmail.com>
2014-02-09 11:58:21 +0100

    65

---
+++ src/main/scala/euler/til70/Euler65.scala
@@ -0,0 +1,34 @@
+package euler
+package til70
+
+import euler._
+
+object Euler65 extends EulerProblem {
+
+  //1,2,1,1,4,1,...,1,2k,1,...
+  def eContFractIterator = new Iterator[Int] {
+    var (i, k) = (1, 0)
+    val hasNext = true
+
+    def next = {
+      i = i + 1
+      if (i % 3 == 0) {
+        k = k + 1
+        2 * k
+      } else 1
+    }
+
+  }
+
+  override def result = {
+    //starting from the 2nd (3)
+    val res = eContFractIterator.take(99).foldRight((BigInt(1), BigInt(0))) {
+      case (x, (num, den)) => (x * num + den, num)
+    }
+
+    val hundredthConvNum = res._2 + 2 * res._1
+    hundredthConvNum.toString.map(_.toString.toInt).sum
+  }
+
+}
+
