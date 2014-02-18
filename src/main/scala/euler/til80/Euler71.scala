7aa3a1988544b96cb3eda747aadf959c1dbf00cd
Giovanni Caporaletti <gcaporaletti@gmail.com>
2014-02-18 15:40:27 +0100

    71

---
+++ src/main/scala/euler/til80/Euler71.scala
@@ -0,0 +1,42 @@
+package euler
+package til80
+
+import euler._
+import scala.annotation.tailrec
+
+object Euler71 extends EulerProblem {
+
+  override def result = {
+    val target = 3.toDouble / 7
+
+    (8 to 1000000) map {
+      d => (binSearch(d, target), d)
+    } filter { // the binary search could produce a non-proper fraction, discard those
+      case (n, d) => mcd(n, d) == 1
+    } maxBy {
+      case (n, d) => n.toDouble / d
+    }
+  }
+
+  /**
+   * binary search for a certain denominator "d"
+   * to determine the greatest n such that n/d < target
+   */
+  def binSearch(d: Int, target: Double) = {
+    @annotation.tailrec
+    def loop(nStart: Int, nEnd: Int): Int = {
+      val n = (nEnd + nStart) / 2
+      if (n == nStart) nStart
+      else if (n.toDouble / d > target) loop(nStart, n)
+      else loop(n, nEnd)
+    }
+    loop(0, d)
+  }
+
+  def mcd(a: Int, b: Int): Int = {
+    val rem = a % b
+    if (rem == 0) b else mcd(b, rem)
+  }
+
+}
+
