183e0e7aade16f814f2975fa62bc320edc23adaf
Giovanni Caporaletti <gcaporaletti@gmail.com>
2014-02-18 17:13:11 +0100

    74

---
+++ src/main/scala/euler/til80/Euler74.scala
@@ -0,0 +1,29 @@
+package euler
+package til80
+
+import scala.annotation.tailrec
+
+object Euler74 extends EulerProblem {
+
+  override def result = {
+    (1 until 1000000).view.par count { chainLen(_) == 60 }
+  }
+
+  def chainLen(n: Int) = {
+    @annotation.tailrec
+    def loop(n: Int, xs: Set[Int]): Int = {
+      val next = n.toDigits.reverse.map(fact).sum;
+      if (xs.contains(next)) xs.size + 1
+      else loop(next, xs + next)
+    }
+    loop(n, Set())
+  }
+
+  def fact(n: Int): Int = {
+    @annotation.tailrec
+    def f(n: Int, acc: Int = 1): Int =
+      if (n <= 1) acc else f(n - 1, acc * n)
+    f(n)
+  }
+}
+
