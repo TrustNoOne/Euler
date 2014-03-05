e538f07629e3348ebe4187a0aff6691aba86feb6
Giovanni Caporaletti <gcaporaletti@gmail.com>
2014-03-05 23:59:08 +0100

    79

---
+++ src/main/scala/euler/til80/Euler79.scala
@@ -0,0 +1,15 @@
+package euler
+package til80
+
+import scala.annotation.tailrec
+
+object Euler79 extends EulerProblem {
+
+  override def result = {
+    // done by hand, simple game...
+    73162890
+  }
+
+}
+
+
