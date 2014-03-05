3ec01b27eb6e1053f99f4a5031bd90440fb0113c
Giovanni Caporaletti <gcaporaletti@gmail.com>
2014-03-05 23:49:40 +0100

    78

---
+++ src/main/scala/euler/til80/Euler78.scala
@@ -0,0 +1,36 @@
+package euler
+package til80
+
+import scala.annotation.tailrec
+
+object Euler78 extends EulerProblem {
+
+  override def result = {
+    val p = Array.ofDim[BigInt](100000)
+    val genPentagonals = Euler76.genPentagonalIterator.toStream
+
+    p(0) = 1
+
+    def loop(n: Int): Int = {
+      if (p(n - 1) % 1000000 == 0) {
+        n - 1
+      } else {
+        val pents = genPentagonals takeWhile (_ <= n)
+        val nextP = pents.foldLeft((BigInt(0), 0)) {
+          case ((res, i), k) =>
+            val sign = if (i / 2 % 2 == 0) 1 else -1
+            (res + sign * p(n - k), i + 1)
+        }
+
+        p(n) = nextP._1
+        loop(n + 1)
+      }
+    }
+
+    loop(1)
+
+  }
+
+}
+
+
