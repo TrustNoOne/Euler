8b189ec78f6c8643668757014afc475f89a36721
Giovanni Caporaletti <gcaporaletti@gmail.com>
2014-09-20 15:37:51 +0100

    92

---
+++ src/main/scala/euler/til100/Euler92.scala
@@ -0,0 +1,21 @@
+package euler
+package til100
+
+object Euler92 extends EulerProblem {
+
+  override def result = {
+    def chainEnd(x: Int, found1: Boolean = false, found89: Boolean = false): Int = x match {
+      case 1 if found1 => 1
+      case 89 if found89 => 89
+      case _ => chainEnd(next(x), found1 || x == 1, found89 || x == 89)
+    }
+
+    (1 until 10000000).par count (chainEnd(_) == 89)
+  }
+
+  // next in the square digit chain
+  def next(x: Int): Int = x.toDigits.fold(0)((sum, d) => sum + d * d)
+
+}
+
+
